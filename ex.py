5fb2ecc3ece6f832ddd6f94f253cecd1517079df
aveczzang <avec@dreamwiz.com>
2018-07-26 14:58:18 +0900

    Module화 작업 추가

---
+++ ex.py
@@ -0,0 +1,13 @@
+import caculator
+
+num1 = caculator.add(10, 2)
+print(num1)
+
+num2 = caculator.sub(10, 2)
+print(num2)
+
+num3 = caculator.mul(10, 2)
+print(num3)
+
+num4 = caculator.div(10, 2)
+print(num4)
